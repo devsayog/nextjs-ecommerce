d6f9afaeaea8cad60f0e88379858d29541574ab1
devsayog <devsayog@gmail.com>
2022-12-15 17:32:21 +0545

    hide flyout menu on dashboard pages

--- src/components/common/layout/Navbar.tsx
+++ src/components/common/layout/Navbar.tsx
@@ -190,18 +190,20 @@ export function Navbar() {
                 <AiOutlineMenu aria-hidden="true" className="h-6 w-6" />
               </button>
             )}
-            {/* FOR User HAMBURGER MENU */}
-            <button
-              type="button"
-              className="fixed bottom-4 left-4 rounded-full bg-slate-900 p-2 drop-shadow-lg transition-all hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-black/75 dark:bg-white dark:focus-visible:ring-white/75 lg:hidden"
-              onClick={() => setOpen(true)}
-            >
-              <div className="sr-only">open admin menu</div>
-              <AiOutlineMenu
-                aria-hidden="true"
-                className="h-6 w-6 text-white dark:text-black"
-              />
-            </button>
+            {/* FOR All User HAMBURGER MENU */}
+            {!router.asPath.includes('/dashboard') ? (
+              <button
+                type="button"
+                className="fixed bottom-4 left-4 rounded-full bg-slate-900 p-2 drop-shadow-lg transition-all hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-black/75 dark:bg-white dark:focus-visible:ring-white/75 lg:hidden"
+                onClick={() => setOpen(true)}
+              >
+                <div className="sr-only">open admin menu</div>
+                <AiOutlineMenu
+                  aria-hidden="true"
+                  className="h-6 w-6 text-white dark:text-black"
+                />
+              </button>
+            ) : null}
 
             <Link
               href="/"
@@ -212,117 +214,119 @@ export function Navbar() {
             </Link>
           </div>
           {/* Flyout menus */}
-          <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
-            <div className="flex h-full space-x-8">
-              {navigation.categories.map((category) => (
-                <Popover key={generateKey()} className="flex">
-                  {({ open }) => (
-                    <>
-                      <div className="relative flex">
-                        <Popover.Button
-                          className={classNames(
-                            open
-                              ? 'border-indigo-600 text-indigo-600'
-                              : 'border-transparent text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100 hover:-translate-y-1',
-                            'relative z-10 -mb-px flex items-center border-b-2 pt-px tracking-wider font-medium transition-colors duration-200 ease-out capitalize',
-                            'focus:outline-none focus-visible:-translate-y-1 transition-all'
-                          )}
-                        >
-                          {category.name}
-                        </Popover.Button>
-                      </div>
+          {!router.asPath.includes('/dashboard') ? (
+            <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
+              <div className="flex h-full space-x-8">
+                {navigation.categories.map((category) => (
+                  <Popover key={generateKey()} className="flex">
+                    {({ open }) => (
+                      <>
+                        <div className="relative flex">
+                          <Popover.Button
+                            className={classNames(
+                              open
+                                ? 'border-indigo-600 text-indigo-600'
+                                : 'border-transparent text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100 hover:-translate-y-1',
+                              'relative z-10 -mb-px flex items-center border-b-2 pt-px tracking-wider font-medium transition-colors duration-200 ease-out capitalize',
+                              'focus:outline-none focus-visible:-translate-y-1 transition-all'
+                            )}
+                          >
+                            {category.name}
+                          </Popover.Button>
+                        </div>
 
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                      >
-                        <Popover.Panel className="absolute inset-x-0 top-full text-sm">
-                          <div
-                            className="absolute inset-0 top-1/2 bg-white shadow dark:bg-slate-900"
-                            aria-hidden="true"
-                          />
-                          <div className="relative bg-white dark:bg-slate-900">
-                            <div className="mx-auto max-w-7xl px-8">
-                              <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
-                                <div className="col-start-2 grid grid-cols-2 gap-x-8">
-                                  {category.featured.map((item) => (
-                                    <div
-                                      key={generateKey()}
-                                      className="group relative text-base sm:text-sm"
-                                    >
-                                      <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                        <img
-                                          src={item.imageSrc}
-                                          alt={item.imageAlt}
-                                          className="object-cover object-center"
-                                        />
-                                      </div>
-                                      <Link
-                                        href={item.href}
-                                        className="mt-6 block font-medium capitalize text-gray-900 dark:text-gray-300"
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-200"
+                          enterFrom="opacity-0"
+                          enterTo="opacity-100"
+                          leave="transition ease-in duration-150"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
+                        >
+                          <Popover.Panel className="absolute inset-x-0 top-full text-sm">
+                            <div
+                              className="absolute inset-0 top-1/2 bg-white shadow dark:bg-slate-900"
+                              aria-hidden="true"
+                            />
+                            <div className="relative bg-white dark:bg-slate-900">
+                              <div className="mx-auto max-w-7xl px-8">
+                                <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
+                                  <div className="col-start-2 grid grid-cols-2 gap-x-8">
+                                    {category.featured.map((item) => (
+                                      <div
+                                        key={generateKey()}
+                                        className="group relative text-base sm:text-sm"
                                       >
-                                        <span
-                                          className="absolute inset-0 z-10"
+                                        <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                                          <img
+                                            src={item.imageSrc}
+                                            alt={item.imageAlt}
+                                            className="object-cover object-center"
+                                          />
+                                        </div>
+                                        <Link
+                                          href={item.href}
+                                          className="mt-6 block font-medium capitalize text-gray-900 dark:text-gray-300"
+                                        >
+                                          <span
+                                            className="absolute inset-0 z-10"
+                                            aria-hidden="true"
+                                          />
+                                          {item.name}
+                                        </Link>
+                                        <p
                                           aria-hidden="true"
-                                        />
-                                        {item.name}
-                                      </Link>
-                                      <p
-                                        aria-hidden="true"
-                                        className="mt-1 text-gray-500 dark:text-gray-400"
-                                      >
-                                        Shop now
-                                      </p>
-                                    </div>
-                                  ))}
-                                </div>
-                                <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
-                                  {category.sections.map((section) => (
-                                    <div key={generateKey()}>
-                                      <p
-                                        id={`${section.name}-heading`}
-                                        className="text-base font-medium capitalize text-gray-900 dark:text-gray-100"
-                                      >
-                                        {section.name}
-                                      </p>
-                                      <ul
-                                        role="list"
-                                        aria-labelledby={`${section.name}-heading`}
-                                        className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                      >
-                                        {section.items.map((item) => (
-                                          <li
-                                            key={generateKey()}
-                                            className="flex"
-                                          >
-                                            <Link
-                                              href={item.href}
-                                              className="inline-block transition hover:text-indigo-800 dark:hover:text-indigo-400"
+                                          className="mt-1 text-gray-500 dark:text-gray-400"
+                                        >
+                                          Shop now
+                                        </p>
+                                      </div>
+                                    ))}
+                                  </div>
+                                  <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
+                                    {category.sections.map((section) => (
+                                      <div key={generateKey()}>
+                                        <p
+                                          id={`${section.name}-heading`}
+                                          className="text-base font-medium capitalize text-gray-900 dark:text-gray-100"
+                                        >
+                                          {section.name}
+                                        </p>
+                                        <ul
+                                          role="list"
+                                          aria-labelledby={`${section.name}-heading`}
+                                          className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
+                                        >
+                                          {section.items.map((item) => (
+                                            <li
+                                              key={generateKey()}
+                                              className="flex"
                                             >
-                                              {item.name}
-                                            </Link>
-                                          </li>
-                                        ))}
-                                      </ul>
-                                    </div>
-                                  ))}
+                                              <Link
+                                                href={item.href}
+                                                className="inline-block transition hover:text-indigo-800 dark:hover:text-indigo-400"
+                                              >
+                                                {item.name}
+                                              </Link>
+                                            </li>
+                                          ))}
+                                        </ul>
+                                      </div>
+                                    ))}
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Popover>
-              ))}
-            </div>
-          </Popover.Group>
+                          </Popover.Panel>
+                        </Transition>
+                      </>
+                    )}
+                  </Popover>
+                ))}
+              </div>
+            </Popover.Group>
+          ) : null}
           {/* Buttons */}
           <ul className="flex items-center gap-1 md:gap-2 2xl:gap-4">
             <li>
